bf6bc92b0e5d15e6cbbd8e1e4bfdf564992de67a
Matthew Sorensen <scholl.ma@gmail.com>
2012-01-02 14:26:05 -0800

    Dynamically builds the links for attribution for all code blocks, using data-url and data-title attrs

---
+++ js/hask.js
@@ -0,0 +1,20 @@
+
+
+
+(function(document){
+    "use strict";
+    var attribute = function(div){
+	var url = div.dataset.url,
+	title = div.dataset.title || url;
+	if(url != ''){
+	    var link = document.createElement('a');
+	    link.setAttribute('href',url);
+	    link.setAttribute('target','_Blank');
+	    link.innerText = title;
+	    div.appendChild(document.createElement('hr'));
+	    div.appendChild(link);
+	}
+    };
+    var blocks = document.getElementsByClassName('code');
+    for(var i = 0; i < blocks.length; i++) attribute(blocks[i]);
+})(document);
