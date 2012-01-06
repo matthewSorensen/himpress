8647203b9d69f1c2a59489c74fb7d828dea60aed
Matthew Sorensen <scholl.ma@gmail.com>
2012-01-06 13:25:25 -0800

    Slides get compiled to a body of text, a list of classes to add, and a list of data attributes

--- Compiler/Compile.hs
+++ Compiler/Compile.hs
@@ -1,18 +1,22 @@
-{-# LANGUAGE TupleSections #-}
-module Compiler.Compile where
+{-# LANGUAGE TupleSections, OverloadedStrings #-}
+module Compiler.Compile (compile)where
 
 import Compiler.Transitions
-import Data.Text (Text)
+import Data.Lenses (fetch)
+import Data.Monoid
+import Data.List (mapAccumL)
+import Data.Text (Text,pack)
+import Data.Map (fromList)
 
+-- Protoslides - ie. slides with a bunch of text and un-composed transitions.
+type PSlide = ([Text],[Transition])
+emptyPSlide = ([],[])
 
-type Slide = ([Text],[Transition])
-emptySlide = ([],[])
-
-splitIntoSlides::[Element]->[Slide]
-splitIntoSlides (l:ls) = out $ foldl split' (l,emptySlide `addToTuple` l,[]) ls
+splitIntoPSlides::[Element]->[PSlide]
+splitIntoPSlides (l:ls) = out $ foldl split' (l,emptyPSlide `addToTuple` l,[]) ls
     where split' (prev,slide,acc) new
               | fusible prev new = (new, addToTuple slide new, acc)
-              | otherwise = (new, emptySlide `addToTuple` new, reverseTuple slide : acc)
+              | otherwise = (new, emptyPSlide `addToTuple` new, reverseTuple slide : acc)
           out (_,_,x) = reverse x
           reverseTuple (a,b) = (reverse a,reverse b)
           addToTuple (a,b) = either ((,b) . (:a)) ((a,) . (:b))
@@ -23,11 +27,32 @@ splitIntoSlides (l:ls) = out $ foldl split' (l,emptySlide `addToTuple` l,[]) ls
           fusible (Left  _) (Left  _) = True
           fusible (Left  _) (Right _) = False
           fusible (Right _) (Left  _) = True
-          fusible (Right _) (Right (Transition _ b)) = b
-
+          fusible (Right _) (Right t) = composes t
+          composes = either snd snd
 -- Now we may compose all of the transitions on each slide, and get a set of
 -- native attributes + a presentation state for each slide
 -- This is then converted to a set of native attributes, and sent off for formating.
+compose::(Int,Int)->[Transition]->(Native,PState)
+compose size = foldl append (mempty,mempty)
+    where append st = either (combineNative st . fst) (combineChange st . fst)
+          combineNative (nat,st) new = (nat `mappend` new,st)
+          combineChange (nat,st) new = (nat, updateState size new st)
+
+toNative::PState->Native
+toNative p = Native {classes = mempty, attrs = fromList [
+                                ("data-scale", str scale p)
+                               ,("data-x", str x p)
+                               ,("data-y", str y p)
+                               ]}
+    where str lens = pack . show . flip fetch lens
+
+type Slide = (Native,Text)
+
+compile::(Int,Int)->[Element]->[Slide]
+compile size = snd . mapAccumL buildSlide mempty . splitIntoPSlides
+    where buildSlide st (body,trans) = let (nat,pstate) = compose size trans
+                                           st' = st `mappend` pstate
+                                       in (st',(nat `mappend` toNative st', mconcat body))
 
 
 -- Critical things to do:
--- Compiler/Transitions.hs
+++ Compiler/Transitions.hs
@@ -3,28 +3,47 @@ module Compiler.Transitions where
 
 import Data.Text
 import Data.Map (Map)
+import Data.Set (Set)
+import Data.Monoid
 import Data.Lenses.Template
+import Data.Lenses (alter)
 
 type Element  = Either Text Transition
+type Transition = Either (Native,Bool) (Change,Bool)
 
-data Transition = Transition Change Bool
-                  deriving(Show,Eq)
-
-data Change = Rotation Int | Move Direction | Scale Int
+data Change = Move Direction | Scale Int
             deriving(Show,Eq)
 
 data Direction = L | R | D | U | Coord (Int,Int)
                  deriving(Show,Eq)
 
-data Native = Native {classes_::[Text],attrs_::Map Text Text}
-              deriving(Show,Eq)
-$(deriveLenses ''Native)
+data Native = Native {classes::Set Text,attrs::Map Text Text}
+            deriving(Show,Eq)
+
+instance Monoid Native where
+    mempty = Native {classes = mempty, attrs = mempty}
+    (Native a b) `mappend` (Native c d) = Native (a `mappend` c) (b `mappend` d)
 
 data PState = PState {
       x_::Int,
       y_::Int,
-      theta_::Int,
       scale_::Int
     } deriving (Show,Eq)
 
 $(deriveLenses ''PState)
+
+instance Monoid PState where
+    mempty = PState 0 0 0
+    (PState a b c) `mappend` (PState d e f) = PState (a+d) (b+e) (c+f)
+
+updateState::(Int,Int)->Change->PState->PState
+updateState _ (Scale i)  = alter scale (+i)
+updateState size (Move dir) = alter x (+dx) . alter y (+dy)
+    where (dx,dy) = sizeOf size dir
+
+sizeOf::(Int,Int)->Direction->(Int,Int)
+sizeOf _ (Coord i) = i
+sizeOf (x,_) L     = (-x,0)
+sizeOf (x,_) R     = ( x,0)
+sizeOf (_,y) D     = (0,-y)
+sizeOf (_,y) U     = (0,y)
