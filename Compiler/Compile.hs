1f650c972151a8d30e903aee42e9922f9327733c
Matthew Sorensen <scholl.ma@gmail.com>
2012-01-06 08:19:00 -0800

    I'm being stupid and not thinking. Wooo stupid people

--- Compiler/Compile.hs
+++ Compiler/Compile.hs
@@ -9,10 +9,10 @@ type Slide = ([Text],[Transition])
 emptySlide = ([],[])
 
 splitIntoSlides::[Element]->[Slide]
-splitIntoSlides l = out $ foldl split' (head l,emptySlide,[]) $ tail l
+splitIntoSlides (l:ls) = out $ foldl split' (l,emptySlide `addToTuple` l,[]) ls
     where split' (prev,slide,acc) new
               | fusible prev new = (new, addToTuple slide new, acc)
-              | otherwise = (new, emptySlide, reverseTuple slide : acc)
+              | otherwise = (new, emptySlide `addToTuple` new, reverseTuple slide : acc)
           out (_,_,x) = reverse x
           reverseTuple (a,b) = (reverse a,reverse b)
           addToTuple (a,b) = either ((,b) . (:a)) ((a,) . (:b))
@@ -28,3 +28,12 @@ splitIntoSlides l = out $ foldl split' (head l,emptySlide,[]) $ tail l
 -- Now we may compose all of the transitions on each slide, and get a set of
 -- native attributes + a presentation state for each slide
 -- This is then converted to a set of native attributes, and sent off for formating.
+
+
+-- Critical things to do:
+-- Step to convert all relative movements to absolute coordinates
+-- Step to compose all composable transitions that aren't separated by anything
+-- Step to turn reduce every (composed) transition to a set of attributes and classes
+-- (perhaps do this at the same time as composing them)
+-- Then turn the list of Either Transition Element to [(Transition,[Element])]
+-- Then format, which is easy enough.
