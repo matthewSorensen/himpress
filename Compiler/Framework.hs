b53b4d0a8f2a6429041f6c67c749b1b44c00ae93
Matthew Sorensen <scholl.ma@gmail.com>
2012-01-05 20:04:13 -0800

    first stab at that. ParseStream mostly works, but... Weird bug with things hanging off the end

--- Compiler/Framework.hs
+++ Compiler/Framework.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE ExistentialQuantification, OverloadedStrings #-}
 module Compiler.Framework where
 
+import Prelude hiding (takeWhile,lookup)
+import Data.Char
 import Compiler.Transitions
 import Data.Attoparsec.Text
 import qualified Data.Text as T
-import Data.Map (Map)
+import Data.Map (Map,lookup,fromList)
 import Control.Applicative
 
 type Text = T.Text
@@ -32,9 +34,7 @@ apply (Mode _ parse form) opts text = either l r form
           l    f = (f parsed, Just text)
           r    f = (f parsed text, Nothing)
 
--- Write the parser for commands
 -- Commands take the form >>> {identifier} {rest of the line is passed to the DocMode}\n
-
 -- All of the text up until the next start of a tag
 text::Parser Text
 text = T.concat . reverse <$> chunks []
@@ -47,4 +47,32 @@ text = T.concat . reverse <$> chunks []
           post acc chunk = case T.splitAt (T.length chunk - 4) chunk of
                              (c,"\\>>>") -> chunks $ ">>>":c:acc
                              (c,_) -> return $ c : acc
-                                          
+-- Parses the body of a command, returning the name and options
+command::Parser (Text,Text)
+command = whitespace *> ((,) <$> name <*> rest) <* endOfLine
+    where whitespace = skipWhile (\c -> isSpace c && c /= '\n')
+          name = takeWhile $ inClass "a-zA-Z0-9-"
+          rest = takeTill (== '\n')
+
+
+
+-- This is moderately difficult.
+parseStream::DocMode->Map Text DocMode->Parser [Element]
+parseStream def modes =  (:) <$> start <*> (reverse <$> rest)
+    where start = fst . apply def "" <$> text
+          rest = (endOfInput *> pure []) <|> (pair <*> rest)
+          neuter p = fst . apply p ""
+          pair = do
+            (name,args) <- command
+            let mode = maybe (error $ "Couldn't find mode " ++ show name) id $ lookup name modes
+            (elem,remain) <- apply mode args <$> text
+            pure $ maybe (elem:) (\x-> ((neuter def $ x):) . (elem:)) remain
+
+testStream = parseStream literal (fromList [("literal",literal)])
+
+{--
+<check if end of input> undefined
+--}
+
+-- Parse text; if we're at the end of input fail
+test p = runParser p . T.pack
