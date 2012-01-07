46e36c79fb7f078db0def17644aae548b0e63c1a
Matthew Sorensen <scholl.ma@gmail.com>
2012-01-06 21:28:09 -0800

    Build the entire document. I really really like blaze-html

--- Compiler/Format.hs
+++ Compiler/Format.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Compiler.Format where
+module Compiler.Format 
+    (FormatOpts (..), defaultOpts, formatPresentation)
+        where
 
 import Compiler.Transitions (Slide,Native (..))
 
@@ -7,18 +9,48 @@ import Data.Set (fold)
 import Data.Map (foldrWithKey)
 import Data.Text (Text)
 import Data.Monoid
-import Prelude hiding (div)
-import Text.Blaze.Html5.Attributes (class_)
-import Text.Blaze.Html5 (div)
-import Text.Blaze (toHtml,toValue,textTag,customAttribute,Html,(!))
+import Control.Monad (mapM_)
+
+import qualified Text.Blaze.Html5 as H5
+import qualified Text.Blaze.Html5.Attributes as A5
+import Text.Blaze 
 import Text.Blaze.Renderer.Utf8 (renderHtml)
 
-(<>)::Monoid a=>a->a->a
-(<>) = mappend
+data FormatOpts = FormatOpts {
+      meta::[(Text,Text)],
+      title::Text,
+      headScripts::[Text],
+      scripts::[Text],
+      style::[Text]
+    }
+
+defaultOpts = FormatOpts {meta = [],
+                          title="",
+                          headScripts = [], 
+                          scripts = ["impress.js","hask.js"],
+                          style = ["style.css"]}
 
 formatSlide::Slide->Html
-formatSlide (nat,body) = applyAttrs nat $ applyClass nat $ div $ toHtml body
+formatSlide (nat,body) = applyAttrs nat $ applyClass nat $ H5.div $ preEscapedText body
     where applyAttrs nat = flip (foldrWithKey addAttr) (attrs nat)
           addAttr name val = (! (customAttribute (textTag name) $ toValue val))
           applyClass nat = (! toClass nat) 
-          toClass = class_ . toValue . fold (((<> " ") .). (<>)) mempty . classes
+          toClass = A5.class_ . toValue . fold ((flip mappend "" .). mappend) mempty . classes
+
+formatPresentation::FormatOpts->[Slide]->Html
+formatPresentation opt slides = 
+    H5.docTypeHtml $ do
+      H5.head $ do
+        H5.meta ! A5.charset "utf-8"
+        H5.title $ toHtml $ title opt
+        mapM_ mkMeta $ meta opt
+        mapM_ (mkStyle  . toValue) $ style opt
+        mapM_ (mkScript . toValue) $ headScripts opt
+      H5.body $ do
+        addImpress $ H5.div $ mapM_ formatSlide slides
+        mapM_ (mkScript . toValue) $ scripts opt
+    where mkStyle s    = H5.link ! A5.rel "stylesheet" ! A5.type_ "text/css" ! A5.href s
+          mkScript s   = (H5.script "") ! A5.type_ "text/javascript" ! A5.src s
+          mkMeta (n,v) = H5.meta ! A5.name (toValue n) ! A5.content (toValue v)
+          addImpress x = x ! A5.id "impress" ! A5.class_ "impress-not-supported"
+
