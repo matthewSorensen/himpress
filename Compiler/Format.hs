7bd68ff9d672ff948ae5a51b9a0cf3b1c652efd0
Matthew Sorensen <scholl.ma@gmail.com>
2012-01-06 21:38:58 -0800

    Add a simplified fallback message

--- Compiler/Format.hs
+++ Compiler/Format.hs
@@ -47,10 +47,15 @@ formatPresentation opt slides =
         mapM_ (mkStyle  . toValue) $ style opt
         mapM_ (mkScript . toValue) $ headScripts opt
       H5.body $ do
-        addImpress $ H5.div $ mapM_ formatSlide slides
+        addImpress $ H5.div $ do
+                       fallback ! A5.class_ "fallback-message"
+                       mapM_ formatSlide slides
         mapM_ (mkScript . toValue) $ scripts opt
     where mkStyle s    = H5.link ! A5.rel "stylesheet" ! A5.type_ "text/css" ! A5.href s
           mkScript s   = (H5.script "") ! A5.type_ "text/javascript" ! A5.src s
           mkMeta (n,v) = H5.meta ! A5.name (toValue n) ! A5.content (toValue v)
           addImpress x = x ! A5.id "impress" ! A5.class_ "impress-not-supported"
 
+fallback = H5.div $ do
+             H5.p "Your browser doesn't support the features required for impress.js"
+             H5.p "(For best results, use the latest WebKit-based browsers)"
