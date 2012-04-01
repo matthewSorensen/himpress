73ce1e21d40467875a2c9f83fdb68b916acf5e86
Matthew Sorensen <scholl.ma@gmail.com>
2012-04-01 00:19:19 -0700

    Totally new interface, and can include scripts and stylesheets

--- Himpress/Format.hs
+++ Himpress/Format.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Himpress.Format 
-    (FormatOpts (..), formatPresentation)
+    (Format (..), File (..), formatPresentation)
         where
 
 import Himpress.Transitions (Slide,Native (..))
@@ -16,22 +16,56 @@ import qualified Text.Blaze.Html5.Attributes as A5
 import Text.Blaze 
 import Text.Blaze.Renderer.Utf8 (renderHtml)
 
+{--
 data FormatOpts = FormatOpts {
       meta::[(Text,Text)],
       title::Text,
-      headScripts::[Text],
-      scripts::[Text],
+      headScripts::[Script],
+      scripts::[Script],
       style::[Text]
     }
+--}
+-- Switch everything over to this.
+data Format = Format {
+      meta::[(Text,Text)]
+    , title::Text
+    , scripts::[File]
+    , style::[File]
+    , bare::Bool
+    }           
+data File = Inline Text | Link Text 
 
-formatSlide::Slide->Html
-formatSlide (nat,body) = applyAttrs nat $ applyClass nat $ H5.div body
-    where applyAttrs nat = flip (foldrWithKey addAttr) (attrs nat)
-          addAttr name val = (! (customAttribute (textTag name) $ toValue val))
-          applyClass nat = (! toClass nat) 
-          toClass = A5.class_ . toValue . intercalate " " . toList . classes
+
+formatPresentation::Format->[Slide]->Html
+formatPresentation o = (if bare o then id else full o) . slideDiv . mapM_ slide
+
+
+full::Format->Html->Html
+full opt content = 
+    H5.docTypeHtml $ do
+      H5.head $ do
+        H5.meta ! A5.charset "utf-8"
+        H5.title $ toHtml $ title opt
+        mapM_ metaTag $ meta opt
+        mapM_ stylesheet $ style opt
+      H5.body $ do
+        fallback ! A5.class_ "fallback-message"
+        content
+        mapM_ script $ scripts opt
+    where metaTag (n,v) = H5.meta ! A5.name (toValue n) ! A5.content (toValue v)
           
+          stylesheet (Link url) = H5.link ! A5.rel "stylesheet" ! A5.type_ "text/css" ! A5.href (toValue url)
+          stylesheet (Inline t) = H5.style (preEscapedText t) ! A5.type_ "text/css"
 
+          script (Inline t) = H5.script (preEscapedText t) ! A5.type_ "text/javascript"
+          script (Link url) = H5.script "" ! A5.type_ "text/javascript" ! A5.src (toValue url)
+
+          fallback = H5.div $ do
+             H5.p "Your browser doesn't support the features required for impress.js"
+             H5.p "(For best results, use the latest WebKit-based browsers)"
+
+    
+          {--
 formatPresentation::FormatOpts->[Slide]->Html
 formatPresentation opt slides = 
     H5.docTypeHtml $ do
@@ -40,18 +74,24 @@ formatPresentation opt slides =
         H5.title $ toHtml $ title opt
         mapM_ mkMeta $ meta opt
         mapM_ (mkStyle  . toValue) $ style opt
-        mapM_ (mkScript . toValue) $ headScripts opt
+        mapM_ script $ headScripts opt
       H5.body $ do
         fallback ! A5.class_ "fallback-message"
-        addImpress $ H5.div $ do
-                       mapM_ formatSlide slides
-        mapM_ (mkScript . toValue) $ scripts opt
+        slideDiv $ mapM_ slide slides
+--        addImpress $ H5.div $ mapM_ formatSlide slides
+        mapM_ script $ scripts opt
         H5.script "impress().init();"  ! A5.type_ "text/javascript"
     where mkStyle s    = H5.link ! A5.rel "stylesheet" ! A5.type_ "text/css" ! A5.href s
-          mkScript s   = H5.script "" ! A5.type_ "text/javascript" ! A5.src s
           mkMeta (n,v) = H5.meta ! A5.name (toValue n) ! A5.content (toValue v)
           addImpress x = x ! A5.id "impress" ! A5.class_ "impress-not-supported"
+--}
 
-fallback = H5.div $ do
-             H5.p "Your browser doesn't support the features required for impress.js"
-             H5.p "(For best results, use the latest WebKit-based browsers)"
+slideDiv::Html->Html
+slideDiv = (! A5.id "impress"). (! A5.class_ "impress-not-supported") . H5.div
+
+slide::Slide->Html
+slide (nat,body) = applyAttrs nat $ applyClass nat $ H5.div body
+    where applyAttrs nat = flip (foldrWithKey addAttr) (attrs nat)
+          addAttr name val = (! (customAttribute (textTag name) $ toValue val))
+          applyClass nat = (! toClass nat) 
+          toClass = A5.class_ . toValue . intercalate " " . toList . classes
--- himpress.hs
+++ himpress.hs
@@ -1,81 +1,30 @@
 {-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
-import Himpress.Framework
+import Himpress.Framework (parsePresentation)
 import Himpress.Compile
 import qualified Himpress.Format as F
 import Himpress.Modes
+import Himpress.Plumbing
+
+import Control.Applicative
 
 import System (getArgs)
-import System.INotify
-import System.Exit
-import Control.Concurrent (threadDelay)
-import Prelude hiding (writeFile,readFile,words,putStrLn,getContents)
-import Data.Text.IO (readFile,getContents) 
-import Data.Text.Lazy.IO (writeFile,putStrLn)
-import Text.Blaze.Renderer.Text (renderHtml)
-import Data.Text (pack,words,Text)
 import System.Console.CmdArgs
+import Text.Blaze.Renderer.Text (renderHtml)
 
-data Opts = Opts {
-      size::(Int,Int)
-    , input::String
-    , output::String
-    , css::String
-    , js::String
-    , impress::String
-    , title::String
-    , watch::String
-    } deriving (Show,Data,Typeable)
 
-opts = Opts {
-         size    = (1000,1000) &= help "Default size (in pixels) for each step"
-       , input   = "" &= typFile
-       , output  = "" &= typFile
-       , css     = "" &= typ listType &= help "CSS files to include in the presentation"
-       , js      = "" &= typ listType &= help "JavaScript files to include in the presentation"
-       , impress = "js/impress.js" &= typFile &= help "Path to impress.js script" 
-       , title   = "" &= typ "STRING" &= help "Title for the presentation"
-       , watch   = "" &= typFile &= help "Recompile file whenever it changes - takes precedence over file"
-       } &= summary "himpress v0.0, (c) Matthew Sorensen 2012"
 
-listType="< space-separated list of paths >"
 
-cmdOptsToFormat c = F.FormatOpts {
-                      F.meta = []
-                    , F.title = pack $ if title c /= "" then title c else output c
-                    , F.headScripts = splitIntoList $ js c
-                    , F.scripts = (:[]) $ pack $ impress c
-                    , F.style = splitIntoList $ css c
-                    } where splitIntoList = words . pack
-
-generatePresentation o = renderHtml . format . comp . parse
-    where format = F.formatPresentation (cmdOptsToFormat o) 
-          comp   = compile $ size o
-          parse  = parsePresentation defaultMode allModes
-
-source::Opts->(Text->IO ())->IO ()
-source o act
-    | watch o /= "" = let w = watch o 
-                      in errorIfSame w (output o) >> watchFile (watch o) act
-    | input o /= "" = readFile (input o) >>= act
-    | otherwise     = getContents >>= act
-
-outputAction o
-    | output o == "" = putStrLn
-    | otherwise      = writeFile $ output o
-
--- This only really watches for the signature events emacs (and presumably that other editor)
--- emits.
-watchFile p act = withINotify $ \inot -> do
-                    addWatch inot [CloseWrite] p $ readAndExecute
-                    loop
-    where readAndExecute _ = readFile p >>= act
-          loop = threadDelay 10000000 >> loop
-errorIfSame a b
-    | a == b = putStrLn "Can't overwrite watched file" >> exitFailure
-    | otherwise = return ()
+generate size fopts = renderHtml . F.formatPresentation fopts . compile size . parse
+    where parse  = parsePresentation defaultMode allModes
 
 main = do
   o <- cmdArgs opts
-  source o $  outputAction o . generatePresentation o
+  formatOpts <- format o
+  let source = inputStream o
+  let sink   = outputStream o
+  watchOrNot o $ (generate (size o) <$> formatOpts <*> source) >>= sink
 
 
+watchOrNot::Opts->IO () -> IO ()
+watchOrNot o act | watch o = watchFiles (dependencies o) act
+                 | otherwise = act
